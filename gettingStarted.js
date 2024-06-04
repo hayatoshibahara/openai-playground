2d988b83a294f8b58f50a53369db9f9c650cc285
Hayato Shibahara <hayatoshibahara@gmail.com>
2024-06-04 16:33:18 +0900

    chore: Improve input

--- gettingStarted.js
+++ gettingStarted.js
@@ -4,11 +4,18 @@ const openai = new OpenAI();
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "面白いアプリの作り方" }],
-    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "あなたは面白いアプリケーションの作り方を教えてくれる伝説的ハッカーです",
+      },
+      { role: "user", content: "面白いアプリの作り方を教えて" },
+    ],
+    model: "gpt-4o",
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
 }
 
 main();
